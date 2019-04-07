a519d84f16d40f0c3699652e9f19954266c7fdc0
Jeff Sandvik <jeff@jeffsandvik.com>
2019-04-07 12:55:39 -0700

    Updates

---
+++ precache-manifest.ccbfc8ff647032cd389eef617823b459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/berserker-frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0225604173037cebb234",
+    "url": "/berserker-frontend/static/js/runtime~main.3e073358.js"
+  },
+  {
+    "revision": "6f333232a0998498ec8e",
+    "url": "/berserker-frontend/static/js/main.8dbcec99.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/berserker-frontend/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6f333232a0998498ec8e",
+    "url": "/berserker-frontend/static/css/main.50966793.chunk.css"
+  },
+  {
+    "revision": "6b0a362e5c1e369835650c35595e07cb",
+    "url": "/berserker-frontend/index.html"
+  }
+];
