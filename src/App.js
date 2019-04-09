cdeec83587f4b9f06519e3fbd0760f0d79208d61
Jeff Sandvik <jeff@jeffsandvik.com>
2019-04-08 20:23:10 -0700

    adds simple filter options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import Table from "react-bootstrap/Table";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,15 +11,23 @@ class App extends Component {
   state = {
     loading: true,
     error: null,
-    moves: []
+    moves: [],
+    characters: [],
+    characterFilter: "",
+    commandFilter: "",
   };
 
-  componentDidMount() {
+  componentDidMount = () => {
     fetch("https://berserkerscience.herokuapp.com/moves")
       .then(response => response.json())
       .then(
         results => {
-          this.setState({ loading: false, moves: results });
+          const characters = results.map(move => move.character.characterName);
+          const uniqueCharacters = characters.filter(
+            (character, index) => characters.indexOf(character) >= index
+          );
+
+          this.setState({ loading: false, moves: results, characters: uniqueCharacters });
         },
         error => {
           this.setState({ loading: false, error });
@@ -23,8 +35,16 @@ class App extends Component {
       );
   }
 
-  render() {
-    const { error, loading, moves } = this.state;
+  onFilterCharacter = (e) => {
+    this.setState({characterFilter: e.target.value});
+  }
+
+  onFilterCommand = (e) => {
+    this.setState({commandFilter: e.target.value});
+  }
+
+  render = () => {
+    const { error, loading, moves, characters, characterFilter, commandFilter } = this.state;
 
     if (error) {
       return <div>Error loading: {error.message}</div>;
@@ -33,33 +53,65 @@ class App extends Component {
     if (loading) {
       return <div>Loading...</div>;
     }
+    console.log(characterFilter);
+
+    let filtered_moves = moves;
+    if (characterFilter) {
+      filtered_moves = filtered_moves.filter(move => characterFilter === move.character.characterName);
+    }
+    if (commandFilter) {
+      filtered_moves = filtered_moves.filter(move => commandFilter === move.command);
+    }
+
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Character</th>
-            <th>Command</th>
-            <th>Impact Frames</th>
-            <th>Blocked Frames</th>
-            <th>Hit Frames</th>
-            <th>Counter Hit Frames</th>
-            <th>Damage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {moves.map(move =>
-            <tr key={move.moveId}>
-              <td>{move.character.characterName}</td>
-              <td>{move.command}</td>
-              <td>{move.impactFrames}</td>
-              <td>{move.blockFrames}</td>
-              <td>{move.hitFrames}</td>
-              <td>{move.counterFrames}</td>
-              <td>{move.damage}</td>
+      <Container>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Filter Character</Form.Label>
+              <Form.Control onChange={this.onFilterCharacter} as="select" value={characterFilter}>
+                <option value="">All</option>
+                {characters.map(character => (
+                  <option key={character}>{character}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Filter Command</Form.Label>
+              <Form.Control onChange={this.onFilterCommand} type="text" placeholder="Example: 5A" value={commandFilter} />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Character</th>
+              <th>Command</th>
+              <th>Impact Frames</th>
+              <th>Blocked Frames</th>
+              <th>Hit Frames</th>
+              <th>Counter Hit Frames</th>
+              <th>Damage</th>
             </tr>
-          )}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {filtered_moves.map(move => (
+              <tr key={move.moveId}>
+                <td>{move.character.characterName}</td>
+                <td>{move.command}</td>
+                <td>{move.impactFrames}</td>
+                <td>{move.blockFrames}</td>
+                <td>{move.hitFrames}</td>
+                <td>{move.counterFrames}</td>
+                <td>{move.damage}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     );
   }
 }
