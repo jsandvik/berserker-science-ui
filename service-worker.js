340807c38d25ddc535c29b66cc0a50c1af9cdf90
Jeff Sandvik <jeff@jeffsandvik.com>
2019-04-18 02:29:33 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/soulcalibur/precache-manifest.ba4358dac0508996ca2363ca32de9b49.js"
+  "/soulcalibur/precache-manifest.bf54f460b9d1b78b22a88778afab931f.js"
 );
 
 workbox.clientsClaim();
